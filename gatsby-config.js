419608950c183fead6aef335bb0554956a14246c
Kimberly <kimberly@adminisatorsmbp.mynetworksettings.com>
2022-07-29 19:01:06 -0400

    Install gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,6 @@ module.exports = {
     title: `Creative Archaeology`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"]
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"],
+  pathPrefix: "/creative-archaeology",
 };
